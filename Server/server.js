7063a8c4f5548e34fd08c7072cb162e19b54fdf8
mracipayam <mracipayam@gmail.com>
2021-10-26 09:29:15 +0300

    server sets

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,17 @@
-const http = require('http');
+var http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log('request made');
+    console.log(req.url , req.method);
+
+    //set header content type
+    res.setHeader('Content-Type','text/html');
+
+    res.write('<head><link rel="stylesheet" href="#"></head>');
+    res.write('<p>hello,murat how are you</p>');
+    res.write('<p>hello,yunus naber </p>');
+    res.end();
+
+
 });
 
 server.listen(3000,'localhost',()=>{

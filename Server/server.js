54a4679d16578165fed5445d00fb6f07167f93b7
mracipayam <mracipayam@gmail.com>
2021-10-26 15:23:01 +0300

    lodash and nodemon packages added

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,20 @@
 var http = require('http');
 const fs = require('fs');
 const { ESRCH } = require('constants');
+const _ = require('lodash');
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url , req.method);
+    
+    //lodash
+    const num = _.random(0,20);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log('hello');
+    });
+
+    greet();
+    greet();
 
     //set header content type
     res.setHeader('Content-Type','text/html');
